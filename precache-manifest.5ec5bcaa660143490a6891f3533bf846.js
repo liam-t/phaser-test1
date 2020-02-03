5d15bcf83d5f973d3d2ebae040068ca3e503dac4
Liam T <liam.t10@icloud.com>
2020-02-03 23:34:38 +0000

    Updates

---
+++ precache-manifest.5ec5bcaa660143490a6891f3533bf846.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dfe611c3222dc259cceb02707b69724",
+    "url": "/phaser-test1/index.html"
+  },
+  {
+    "revision": "df7632d0055e9efdc412",
+    "url": "/phaser-test1/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "faa40ba3d68f8ea6ccf9",
+    "url": "/phaser-test1/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "df7632d0055e9efdc412",
+    "url": "/phaser-test1/static/js/2.b5eaf396.chunk.js"
+  },
+  {
+    "revision": "9847d45c2b5cf2b52d386d7984429e39",
+    "url": "/phaser-test1/static/js/2.b5eaf396.chunk.js.LICENSE"
+  },
+  {
+    "revision": "faa40ba3d68f8ea6ccf9",
+    "url": "/phaser-test1/static/js/main.0c41ee01.chunk.js"
+  },
+  {
+    "revision": "2cd815de71c070050b85",
+    "url": "/phaser-test1/static/js/runtime-main.bc99d83f.js"
+  },
+  {
+    "revision": "2ccf454335dfe5608b73a03bcb968941",
+    "url": "/phaser-test1/static/media/bg.png.2ccf4543.img"
+  },
+  {
+    "revision": "ac8b3f25e8d069e9ea8633d90dc06893",
+    "url": "/phaser-test1/static/media/buildings.png.ac8b3f25.img"
+  },
+  {
+    "revision": "94b1cb8861f96f7fe393c61fcdd7225a",
+    "url": "/phaser-test1/static/media/far-buildings.png.94b1cb88.img"
+  },
+  {
+    "revision": "2b9bb58deccc68c9d15b40d27a83e854",
+    "url": "/phaser-test1/static/media/foreground.png.2b9bb58d.img"
+  },
+  {
+    "revision": "552be6d484b39ba90413a5e7bf6f063d",
+    "url": "/phaser-test1/static/media/ryu1.552be6d4.atlas"
+  },
+  {
+    "revision": "591d7afc63098d18e79e73b540ed9d4e",
+    "url": "/phaser-test1/static/media/ryu1.591d7afc.spr"
+  }
+]);
